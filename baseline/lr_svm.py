c3e4d0bdb91dcae784b22bbc28fab4e71fcd8f7c
Jing <liujingyun0209@gmail.com>
2017-11-01 10:17:42 -0400

    vanilla CNN model

--- baseline/lr_svm.py
+++ baseline/lr_svm.py
@@ -12,10 +12,10 @@ if __name__=="__main__":
     model = SVC()
     
     X,y = load_data(p=p)
-    X_train, X_valid = X[:0.1*(X.shape[0])], X[0.1*(X.shape[0]):]
-    y_train, y_valid = y[:0.1*(X.shape[0])], y[0.1*(X.shape[0]):]
-    model.fit(X_train.reshape((-1,4096)),y_train)
-    print("model score on validation set",model.score(X_valid,y_valid))
+    X_train, X_valid = X[:int(0.1*(X.shape[0]))], X[int(0.1*(X.shape[0])):]
+    y_train, y_valid = y[:int(0.1*(X.shape[0]))], y[int(0.1*(X.shape[0])):]
+    model.fit(X_train.reshape((-1,4096)),y_train.reshape((-1,)))
+    print("model score on validation set",model.score(X_valid.reshape((-1,4096)),y_valid.reshape((-1,))))
 
     # x_test = load_test_data(p=p)
     # yp = model.predict()
