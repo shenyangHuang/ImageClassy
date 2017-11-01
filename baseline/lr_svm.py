120a713ea9ea72463617992f056b93307cbdc80f
Jingyun <jingyun.liu@mail.mcgill.ca>
2017-11-01 10:16:55 -0400

    working baselines

--- baseline/lr_svm.py
+++ baseline/lr_svm.py
@@ -1,6 +1,6 @@
 import sys
 from sklearn.linear_model import LogisticRegression
-from sklearn.svm import SVC
+from sklearn.svm import SVC, SVR
 
 sys.path.append('../')
 from utils import *
@@ -9,14 +9,14 @@ if __name__=="__main__":
     p="../../" # path to data files
 
     # model = LogisticRegression()
-    model = SVC()
+    model = SVR()
     
     X,y = load_data(p=p)
-    X_train, X_valid = X[:0.1*(X.shape[0])], X[0.1*(X.shape[0]):]
-    y_train, y_valid = y[:0.1*(X.shape[0])], y[0.1*(X.shape[0]):]
-    model.fit(X_train.reshape((-1,4096)),y_train)
-    print("model score on validation set",model.score(X_valid,y_valid))
 
+    X_train, X_valid = X[:int(0.1*(X.shape[0]))], X[int(0.1*(X.shape[0])):]
+    y_train, y_valid = y[:int(0.1*(X.shape[0]))], y[int(0.1*(X.shape[0])):]
+    model.fit(X_train.reshape((-1,4096)),y_train.reshape((-1,)))
+    print("model score on validation set",model.score(X_valid.reshape((-1,4096)),y_valid.reshape((-1,))))
     # x_test = load_test_data(p=p)
     # yp = model.predict()
     # write_prediction(yp)
